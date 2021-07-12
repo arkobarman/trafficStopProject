c813d39cac99f351c860054c1b87cde59e1d41f5
arkobarman <arkoju@gmail.com>
2021-07-12 12:30:11 -0500

    added description for county type

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 import time
 import utils
 import datetime as dt
@@ -16,7 +17,8 @@ class TrafficStopData:
 		df['violation'] = [s.lower() for s in df['violation']]
 		self.df = utils.remove_empty_rows(df, 'subject_race')
 		print('loading time: %d'%(time.time()-start))
-		county_df = pd.read_csv('2014-2018.csv')
+        # Load county categories downloaded from US OMB website
+		county_df = pd.read_csv(os.path.join('data', state, '2014-2018.csv'))
 		county_df = county_df[county_df['State']=='Texas']
 		self.county_df = county_df.filter(items=['Metropolitan Status', 'County Name'])
 
@@ -24,6 +26,10 @@ class TrafficStopData:
 		"""
 		preproccesses df in place
 		"""
+        
+        # Convert date and time to a single column which indicates time elapsed yet in the year
+        # This is scaled from 0-1
+        # This gives us a continuous numeric column
 		start = time.time()
 		self.df = self.df[self.df['violation'].str.contains('speed', regex=False)]
 		self.df.drop('violation', 1, inplace=True)
@@ -38,12 +44,15 @@ class TrafficStopData:
 		scaler.fit(np.array(self.df['time']).reshape(-1,1))
 		self.df['time'] = scaler.transform(np.array(self.df['time']).reshape(-1,1))
 		self.df.drop(['hour', 'minute', 'date'], 1, inplace=True)
-
+        
+        # County names are converted to county type - metropolitan, micropolitan or non-core
+        # For definitions, see US OMB website
 		self.df['county_name'] = [name[:-7] for name in self.df['county_name']]
 		self.df = self.df.join(self.county_df.set_index('County Name'), on='county_name')
 		self.df.drop('county_name', 1, inplace=True)
 		self.df.rename(columns={'Metropolitan Status':'county_type'}, inplace=True)
 
+        # Convert citation issued and warning issued columns to integer
 		self.df = self.df.astype({'citation_issued': 'int64', 'warning_issued': 'int64'})
 		self.df = pd.get_dummies(self.df, dummy_na=True)
 		print('preprocessing time: %d'%(time.time()-start))
--- utils.py
+++ utils.py
@@ -56,5 +56,62 @@ def autolabel(rects, ax, format_str):
                     ha='center', va='bottom',
                     fontsize=16)
 
+def plot_ROC_confidence_intervals(y_true, y_pred, n_bootstraps=100, rng_seed=2021, conf_interv = 0.9):
+    
 
+    print("Original ROC area: {:0.3f}".format(roc_auc_score(y_true, y_pred)))
+
+    bootstrapped_scores = []
+    bootRocLst = []
+
+    rng = np.random.RandomState(rng_seed)
+    
+    # create fpr grid for interpolation
+    fprGridVec = np.linspace(0, 1, 100, endpoint=True)
+    # matrix containing all tpr corresponding to fprGridVec
+    tprGridMat = np.zeros((len(fprGridVec), n_bootstraps))
+    
+    for i in range(n_bootstraps):
+        # bootstrap by sampling with replacement on the prediction indices
+        indices = rng.randint(0, len(y_pred), len(y_pred))
+        if len(np.unique(y_true[indices])) < 2:
+            # We need at least one positive and one negative sample for ROC AUC
+            # to be defined: reject the sample
+            continue
+
+        score = roc_auc_score(y_true[indices], y_pred[indices])
+        tmpFpr, tmpTpr, _ = roc_curve(y_true[indices], y_pred[indices])
+        tmpFpr = np.concatenate(([0], tmpFpr, [1]))
+        tmpTpr = np.concatenate(([0], tmpTpr, [1]))
+
+        # interpolate for comparable ROCs
+        fInter = scipy.interpolate.interp1d(tmpFpr, tmpTpr, kind='nearest')
+        tprGridMat[:, i] = fInter(fprGridVec)
+
+        bootstrapped_scores.append(score)
+        bootRocLst.append([tmpFpr, tmpTpr])
+    #     print("Bootstrap #{} ROC area: {:0.3f}".format(i + 1, score))
+
+    sorted_scores = np.array(bootstrapped_scores)
+    sorted_scores.sort()
+
+    # Computing the lower and upper bound of the 90% confidence interval
+    # You can change the bounds percentiles to 0.025 and 0.975 to get
+    # a 95% confidence interval instead.
+    confidence_lower = sorted_scores[int((1 - conf_interv)/2 * len(sorted_scores))]
+    confidence_upper = sorted_scores[int((1 - (1 - conf_interv)/2) * len(sorted_scores))]
+    print("Confidence interval for the score: [{:0.3f} - {:0.3}]".format(confidence_lower, confidence_upper))
+    
+    # confidence interval for ROC
+    tprGridMatS = np.sort(tprGridMat, axis=1)
+    tprLow025 = tprGridMatS[:, int(0.025 * n_bootstraps)]
+    tprTop975 = tprGridMatS[:, int(0.975 * n_bootstraps)]
+    tprMean = np.mean(tprGridMat, axis=1)
+
+    # plt.hold(True)
+    ax = plt.gca()  # kwargs.pop('ax', plt.gca())
+    base_line, = ax.plot(fprGridVec, tprMean, '-', linewidth=4)
+    ax.fill_between(fprGridVec, tprLow025, tprTop975, facecolor=base_line.get_color(), alpha=0.2)
+
+    
 
