d50e9b8076235cb8a6c99a065dd5e27a4cafb099
Minxuan Luo <89892735+minxuanluo@users.noreply.github.com>
2023-08-25 14:52:28 -0500

    Delete Conversion_parquet_file.R

--- Conversion_parquet_file.R
+++
@@ -1,8 +0,0 @@
-install.packages("arrow")
-library('arrow')
-
-df <- readRDS("C:/Users/tejes/OneDrive/Desktop/traffic_stop.rds")
-write_parquet(df, sink = 'C:/Users/tejes/OneDrive/Desktop/traffic.parquet')
-
-
-
