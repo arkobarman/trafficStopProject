c213f68355a50723a4ea42acfdd914869f8ed323
arkobarman <arkoju@gmail.com>
2025-07-28 15:52:08 -0500

    serious and nonserious stops

--- Conversion_parquet_file.R
+++ Conversion_parquet_file.R
@@ -1,8 +1,9 @@
-install.packages("arrow")
+install.packages("arrow", dependencies = TRUE)
 library('arrow')
 
-df <- readRDS("C:/Users/tejes/OneDrive/Desktop/traffic_stop.rds")
-write_parquet(df, sink = 'C:/Users/tejes/OneDrive/Desktop/traffic.parquet')
+
+df <- readRDS("data/yg821jf8611_tx_statewide_2020_04_01.rds")
+write_parquet(df, sink = 'data/traffic.parquet')
 
 
 
